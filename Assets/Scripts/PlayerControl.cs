4cf3744951a4d03f86d03c4f8af79c9e56e366db
Oliver Lützhøft Enemærke <Einmarx@users.noreply.github.com>
2018-03-17 00:03:29 +0100

    Added player control

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) ;
+        {
+            Vector3 jump = new Vector3(0.0f, 10f, 0.0f);
+            rb.AddForce(jump);
+
+        }
+    }
+
+   
+}
