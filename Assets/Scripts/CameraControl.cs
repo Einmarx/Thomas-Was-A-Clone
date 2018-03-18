928e547583089d4979307d8809251361669f25e6
Oliver Lützhøft Enemærke <Einmarx@users.noreply.github.com>
2018-03-18 03:34:45 +0100

    Camera Control & Smoothing

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject player;
+
+    public Transform target;
+
+    public float SmoothTime = 0.3F;
+
+    private Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -5));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
+	}
+}
